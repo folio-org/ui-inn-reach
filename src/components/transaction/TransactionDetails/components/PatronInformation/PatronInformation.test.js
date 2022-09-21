4bc3e33f5b35a80957700c6bbec0c7f597144415
Priyanka <priyanka_terala@epam.com>
2022-09-21 12:45:10 +0530

    increade test coverage

--- src/components/transaction/TransactionDetails/components/PatronInformation/PatronInformation.test.js
+++ src/components/transaction/TransactionDetails/components/PatronInformation/PatronInformation.test.js
@@ -6,6 +6,11 @@ import { Router } from 'react-router';
 import { translationsProperties } from '../../../../../../test/jest/helpers';
 import PatronInformation from './PatronInformation';
 
+// jest.mock('react-router-dom', () => ({
+//   ...jest.requireActual('react-router-dom'),
+//   Link: jest.fn(() => <div>Users</div>)
+// }));
+
 const transactionMock = {
   centralServerCode: 'd2ir',
   hold: {
@@ -19,9 +24,7 @@ const transactionMock = {
 
 const history = createMemoryHistory();
 
-const renderPatronInformation = ({
-  transaction = transactionMock,
-} = {}) => {
+const renderPatronInformation = (transaction) => {
   return renderWithIntl(
     <Router history={history}>
       <PatronInformation
@@ -33,23 +36,52 @@ const renderPatronInformation = ({
 };
 
 describe('PatronInformation', () => {
-  beforeEach(() => {
-    renderPatronInformation();
+  describe('should render Patron Information', () => {
+    beforeEach(() => {
+      renderPatronInformation(transactionMock);
+    });
+
+    it('should show the transaction patron id', () => {
+      expect(screen.getByText('patron1')).toBeVisible();
+    });
+
+    it('should show the transaction patron name', () => {
+      expect(screen.getByText('Brown, Adam')).toBeVisible();
+    });
+
+    it('should show the transaction patron type', () => {
+      expect(screen.getByText('d2ir: 200')).toBeVisible();
+    });
+
+    it('should show the transaction patron agency', () => {
+      expect(screen.getByText('jcg01')).toBeVisible();
+    });
   });
 
-  it('should show the transaction patron id', () => {
-    expect(screen.getByText('patron1')).toBeVisible();
+  describe('render patron information with link in patron id', () => {
+    beforeEach(() => {
+      renderPatronInformation({ ...transactionMock, type: 'PATRON' });
+    });
+
+    it('should show the transaction patron id as a link', () => {
+      expect(screen.getByRole('link')).toBeDefined();
+    });
   });
 
-  it('should show the transaction patron name', () => {
-    expect(screen.getByText('Brown, Adam')).toBeVisible();
-  });
+  describe('render patron information with patron id as just a text', () => {
+    beforeEach(() => {
+      renderPatronInformation({ ...transactionMock, type: 'ITEM' });
+    });
 
-  it('should show the transaction patron type', () => {
-    expect(screen.getByText('d2ir: 200')).toBeVisible();
-  });
+    it('should show the transaction patron id is not a link', () => {
+      const links = screen.queryAllByRole('link');
 
-  it('should show the transaction patron agency', () => {
-    expect(screen.getByText('jcg01')).toBeVisible();
+      expect(links.length).toBe(0);
+      expect(screen.getByText('patron1')).toBeVisible();
+    });
+
+    it('should show the transaction patron id is a text', () => {
+      expect(screen.getByText('patron1')).toBeVisible();
+    });
   });
 });
