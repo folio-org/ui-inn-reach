87407634019a638dfb7739ededad5fc9c979315e
Priyanka Terala <104053200+Terala-Priyanka@users.noreply.github.com>
2022-10-26 16:29:07 +0530

    UIINREACH-191 -  components are incorrectly imported directly from stripes-* packages (#255)
    * UIINREACH-191 correct import path from stripes components
    
    * UIINREACH-191 corrected two imports
    
    * removed all code smells
    
    * UIINREACH-191 - components are incorrectly imported directly from stripes-* packages
    
    * UIINREACH-191 - modified import
    
    * UIINREACH-191 update import to named import
    
    * update mock
    
    * UIINREACH-191 - revert changes to mock
    
    * UIINREACH-191 - components are incorrectly imported directly from stripes-* packages

--- src/settings/routes/CentralServersConfigurationRoute/CentralServersConfigurationRootLayer.js
+++ src/settings/routes/CentralServersConfigurationRoute/CentralServersConfigurationRootLayer.js
@@ -7,7 +7,7 @@ import { FormattedMessage } from 'react-intl';
 
 import {
   stripesConnect,
-} from '@folio/stripes-core';
+} from '@folio/stripes/core';
 
 import CentralServersConfigurationContext from '../../../contexts/CentralServersConfigurationContext';
 import {
